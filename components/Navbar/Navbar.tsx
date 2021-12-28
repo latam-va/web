caf98c49426341a2b33fc93430b9c1c193b6f663
Francisco Gonzalez Cabana <frangc0206@gmail.com>
2021-12-28 16:09:04 -0300

    New Navbar

---
+++ components/Navbar/Navbar.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Navbar = () => {
+    return (
+        <div className='w-full h-24 bg-orange-300'>
+            
+        </div>
+    )
+}
+
+export default Navbar
