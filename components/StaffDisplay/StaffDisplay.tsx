e0b30a173f37361275ff89d51eab684a34385fd1
Francisco Gonzalez Cabana <frangc0206@gmail.com>
2022-01-04 22:12:41 -0300

    Image Change

--- components/StaffDisplay/StaffDisplay.tsx
+++ components/StaffDisplay/StaffDisplay.tsx
@@ -2,12 +2,14 @@ import React from 'react'
 import Image from 'next/image'
 import test from '../../public/images/test.jpg'
 import StaffCard from '../../lib/StaffCard'
+import ceo from '../../public/images/staff/ceo.jpg'
+import web from '../../public/images/staff/web.jpg'
 
 const StaffDisplay = () => {
     return (
         <div className='bg-zinc-100 w-full min-h-screen flex justify-center items-start'>
-            <StaffCard name='Sebastian' job='CEO' image={test} mail='ceo@latamva.com.ar' />
-            <StaffCard name='Francisco' job='WEB, RRPP' image={test} mail='web@latamva.com.ar' />
+            <StaffCard name='Sebastian' job='CEO' image={ceo} mail='ceo@latamva.com.ar' />
+            <StaffCard name='Francisco' job='WEB, RRPP' image={web} mail='web@latamva.com.ar' />
             <StaffCard name='Francisco' job='Tours y Eventos' image={test} mail='rrpp@latamva.com.ar' />
         </div>
     )
