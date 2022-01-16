51971240905dbbf9d8168e7c481e78925499c836
Francisco Gonzalez Cabana <frangc0206@gmail.com>
2022-01-16 13:05:34 -0300

    Added Locales to cfg

--- next.config.js
+++ next.config.js
@@ -3,5 +3,9 @@ module.exports = {
   reactStrictMode: true,
   images: {
     domains: ['github.com', 'raw.githubusercontent.com']
+  },
+  i18n: {
+    locales: ["en-UK", "es-AR"],
+    defaultLocale: "es-AR"
   }
 }
