baa091df3d386f66fd79340d352d9eed9f24453c
Francisco Gonzalez <frangc0206@gmail.com>
2022-01-28 23:48:25 -0300

    Added image domain

--- next.config.js
+++ next.config.js
@@ -2,7 +2,7 @@
 module.exports = {
   reactStrictMode: true,
   images: {
-    domains: ['github.com', 'raw.githubusercontent.com']
+    domains: ['github.com', 'raw.githubusercontent.com', 'www.latamva.com.ar', 'latamva.com.ar']
   },
   i18n: {
     locales: ["en-UK", "es-AR"],
