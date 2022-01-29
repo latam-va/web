eda4f1acfe87290c29b0e84f3a60f93de487f2e4
Francisco Gonzalez <frangc0206@gmail.com>
2022-01-28 23:48:17 -0300

    Changed image locations

--- components/Footer/Footer.tsx
+++ components/Footer/Footer.tsx
@@ -3,27 +3,37 @@ import Image from "next/image";
 import partner from "../../public/images/partner.png";
 import ar from "../../public/images/ar.png";
 import FooterCol from "../../lib/FooterCol";
- 
-import { SocialIcon } from 'react-social-icons'
+
+import { SocialIcon } from "react-social-icons";
 
 const Footer = () => {
   return (
-    <div className="flex flex-col w-full h-full bg-gradient-to-r from-main to-secondary justify-start items-center sm:flex-row">
+    <div className="flex flex-col w-full h-full bg-gradient-to-r from-main to-secondary justify-between items-center sm:flex-row">
       <div className="flex flex-col m-10">
-        <Image src={ar} alt="partner" width={"300"} height={"112"} />
+        <Image src={ar} alt="partner" width={"280"} height={"112"} />
       </div>
-      <div className="flex flex-col m-10">
-        <Image src={partner} alt="partner" width={"300"} height={"100"} />
-      </div>
+
       <div className="flex flex-col m-10">
         <h1 className="font-semibold text-2xl text-center text-slate-100 mb-5">
           LATAM Argentina Virtual © 2022
         </h1>
         <div className="flex flex-row items-center justify-between">
-          <SocialIcon className="mr-2" url="https://twitter.com/LatamArgVirtual" />
-          <SocialIcon className="mr-2" url="https://instagram.com/latamva_arg" />
-          <SocialIcon className="mr-2" url="https://www.facebook.com/LATAM-Argentina-Virtual-110989594796657/" />
+          <SocialIcon
+            className="mr-2"
+            url="https://twitter.com/LatamArgVirtual"
+          />
+          <SocialIcon
+            className="mr-2"
+            url="https://instagram.com/latamva_arg"
+          />
+          <SocialIcon
+            className="mr-2"
+            url="https://www.facebook.com/LATAM-Argentina-Virtual-110989594796657/"
+          />
         </div>
+      </div>
+      <div className="flex flex-col m-10">
+        <Image src={partner} alt="partner" width={"270"} height={"100"} />
       </div>
     </div>
   );
